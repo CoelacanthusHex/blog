8dc8a8c02c349f3a3c27949561b380792d97a674
Coelacanthus <liuhongwu2003@gmail.com>
2020-03-01 04:54:10 +0800

    Deploy to IPFS

--- sw-template.js
+++ sw-template.js
@@ -1,57 +1,61 @@
-const workboxVersion = '4.3.1';
-
-importScripts(`https://cdn.jsdelivr.net/npm/workbox-cdn@${workboxVersion}/workbox/workbox-sw.js`);
-
-workbox.setConfig({
-    modulePathPrefix: `https://cdn.jsdelivr.net/npm/workbox-cdn@${workboxVersion}/workbox/`
-});
-
+const workboxVersion = '5.0.0';
+importScripts(`https://storage.googleapis.com/workbox-cdn/releases/${workboxVersion}/workbox-sw.js`);
 workbox.core.setCacheNameDetails({
-    prefix: "lhw's blog"
+    prefix: "Coelacanthus"
 });
-
 workbox.core.skipWaiting();
-
 workbox.core.clientsClaim();
-
-workbox.precaching.precacheAndRoute([]);
-
+workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
 workbox.precaching.cleanupOutdatedCaches();
-
 // Images
 workbox.routing.registerRoute(
     /\.(?:png|jpg|jpeg|gif|bmp|webp|svg|ico)$/,
     new workbox.strategies.CacheFirst({
         cacheName: "images",
         plugins: [
-            new workbox.expiration.Plugin({
+            new workbox.expiration.ExpirationPlugin({
                 maxEntries: 1000,
                 maxAgeSeconds: 60 * 60 * 24 * 30
             }),
-            new workbox.cacheableResponse.Plugin({
+            new workbox.cacheableResponse.CacheableResponsePlugin({
                 statuses: [0, 200]
             })
         ]
     })
 );
-
+// Videos
+workbox.routing.registerRoute(
+    /\.(?:mp4|webm|ogg)$/,
+    new workbox.strategies.CacheFirst({
+        cacheName: "videos",
+        plugins: [
+            new workbox.expiration.ExpirationPlugin({
+                maxEntries: 1000,
+                maxAgeSeconds: 60 * 60 * 24 * 30
+            }),
+            new workbox.cacheableResponse.CacheableResponsePlugin({
+                statuses: [200]
+            }),
+            new workbox.rangeRequests.RangeRequestsPlugin()
+        ]
+    })
+);
 // Fonts
 workbox.routing.registerRoute(
     /\.(?:eot|ttf|woff|woff2)$/,
     new workbox.strategies.CacheFirst({
         cacheName: "fonts",
         plugins: [
-            new workbox.expiration.Plugin({
+            new workbox.expiration.ExpirationPlugin({
                 maxEntries: 1000,
                 maxAgeSeconds: 60 * 60 * 24 * 30
             }),
-            new workbox.cacheableResponse.Plugin({
+            new workbox.cacheableResponse.CacheableResponsePlugin({
                 statuses: [0, 200]
             })
         ]
     })
 );
-
 // Google Fonts
 workbox.routing.registerRoute(
     /^https:\/\/fonts\.googleapis\.com/,
@@ -64,50 +68,30 @@ workbox.routing.registerRoute(
     new workbox.strategies.CacheFirst({
         cacheName: 'google-fonts-webfonts',
         plugins: [
-            new workbox.expiration.Plugin({
+            new workbox.expiration.ExpirationPlugin({
                 maxEntries: 1000,
                 maxAgeSeconds: 60 * 60 * 24 * 30
             }),
-            new workbox.cacheableResponse.Plugin({
+            new workbox.cacheableResponse.CacheableResponsePlugin({
                 statuses: [0, 200]
             })
         ]
     })
 );
-
 // Static Libraries
 workbox.routing.registerRoute(
     /^https:\/\/cdn\.jsdelivr\.net/,
     new workbox.strategies.CacheFirst({
         cacheName: "static-libs",
         plugins: [
-            new workbox.expiration.Plugin({
+            new workbox.expiration.ExpirationPlugin({
                 maxEntries: 1000,
                 maxAgeSeconds: 60 * 60 * 24 * 30
             }),
-            new workbox.cacheableResponse.Plugin({
+            new workbox.cacheableResponse.CacheableResponsePlugin({
                 statuses: [0, 200]
             })
         ]
     })
 );
-
-// External Images
-workbox.routing.registerRoute(
-    /^https:\/\/raw\.githubusercontent\.com\/reuixiy\/hugo-theme-meme\/master\/static\/icons\/.*/,
-    new workbox.strategies.CacheFirst({
-        cacheName: "external-images",
-        plugins: [
-            new workbox.expiration.Plugin({
-                maxEntries: 1000,
-                maxAgeSeconds: 60 * 60 * 24 * 30
-            }),
-            new workbox.cacheableResponse.Plugin({
-                statuses: [0, 200]
-            })
-        ]
-    })
-);
-
-workbox.googleAnalytics.initialize({});
- 
+workbox.googleAnalytics.initialize();
