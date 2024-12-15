4db268a689e20a7bd9b8932fa4aa23d88dd51dc9
Coelacanthus <uwu@coelacanthus.name>
2024-12-16 06:39:44 +0800

    Be compatible with old icon income links

--- build.sh
+++ build.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
+# For compatibility with old icon income links.
+pushd static
+    ln -sfr avatar.min.svg icon.svg
+    ln -sfr avatar.png icon.png
+    ln -sfr avatar.webp icon.webp
+    ln -sfr avatar.avif icon.avif
+    ln -sfr avatar.jxl icon.jxl
+popd
+
 yarn run build #&& yarn run ipfs-deploy
