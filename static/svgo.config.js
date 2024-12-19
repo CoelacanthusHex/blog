9bc06ca13e62c3503fa56188a684b5c597302e10
Coelacanthus <uwu@coelacanthus.name>
2024-12-20 06:20:25 +0800

    add viewBox to make SVG actual scalable

---
+++ static/svgo.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  multipass: true,
+  plugins: [
+    {
+      name: 'preset-default',
+      params: {
+        overrides: {
+          // viewBox is required to resize SVGs with CSS.
+          // Remove it make SVG become VG!!!
+          // @see https://github.com/svg/svgo/issues/1128
+          // FIXME: This change has been included in 4.0.0
+          // @see https://github.com/svg/svgo/pull/1461
+          removeViewBox: false,
+        },
+      },
+    },
+  ],
+};
