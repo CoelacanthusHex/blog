6b29f7b5b80308a0a2bca72a1b20ecf9e9832002
Coelacanthus <uwu@coelacanthus.name>
2024-12-20 06:36:26 +0800

    conform BIMI spec

--- static/svg-variant.sh
+++ static/svg-variant.sh
@@ -3,6 +3,7 @@
 sed 's|<g>|<g transform="translate(256 256) scale(0.8, 0.8) translate(-256 -256)">|' avatar.svg > avatar-round.svg
 sed -E -e '/\tversion="1.1"/d' \
        -e '/<svg/a \\tversion="1.2"\n\tbaseProfile="tiny-ps"' \
+       -e '/<desc>/{N; N; d}' \
     avatar-round.svg > avatar-bimi.svg
 
 svgo --multipass -i avatar.svg       -o avatar.min.svg
