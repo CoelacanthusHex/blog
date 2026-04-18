15fadf58f4f75a6920dd072f9e7fb7615463b27d
Coelacanthus <uwu@coelacanthus.name>
2026-04-19 07:02:20 +0800

    add more detailed metadata to avatar

--- build.sh
+++ build.sh
@@ -5,7 +5,6 @@ pushd static
     ln -sfr avatar.min.svg icon.svg
     ln -sfr avatar.png icon.png
     ln -sfr avatar.webp icon.webp
-    ln -sfr avatar.avif icon.avif
     ln -sfr avatar.jxl icon.jxl
 popd
 
